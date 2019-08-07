488ca3f6fb1aa0ae4ced75be23dcb24ba6fbe9cc
Isaias Melo <isaiasmelo@MacBook-Pro-de-Isaias.local>
2019-08-07 09:45:52 -0300

    ajustes finais

--- components/Prospecto.js
+++ components/Prospecto.js
@@ -7,7 +7,7 @@ import {
 	Dimensions,
 } from 'react-native';
 import { Card, Icon, } from 'react-native-elements'
-import { white, gray, primary, yellow, gold, lightdark } from '../helpers/colors'
+import { white, gray, primary, yellow, gold, lightdark, black, red } from '../helpers/colors'
 import call from 'react-native-phone-call'
 import {
 	alterarProspectoNoAsyncStorage,
@@ -29,6 +29,10 @@ import {
 
 class Prospecto extends React.Component {
 
+	state = {
+		mostrarOpcoes: false
+	}
+
 	chamarOTelefoneDoCelular() {
 		const { prospecto } = this.props
 		call({ number: prospecto.telefone, prompt: false }).catch(console.error)
@@ -40,6 +44,7 @@ class Prospecto extends React.Component {
 
 	render() {
 		const { prospecto, navigation } = this.props
+		const { mostrarOpcoes } = this.state
 
 		let parametros = montarObjetoParaPerguntas(prospecto.situacao_id)
 		parametros.prospecto_id = prospecto._id
@@ -56,23 +61,13 @@ class Prospecto extends React.Component {
 			}
 			const medalha = {
 				icone,
-				cor: prospecto.situacao_id >= x ? primary : '#AAAAAA',
+				cor: prospecto.situacao_id >= x ? gold : '#AAAAAA',
 			}
 			listaDeMedalhas.push(medalha)
 		}
 
 		return (
 			<Card containerStyle={stylesProspecto.containerCard} key={prospecto.id}>
-				<View style={[stylesProspecto.containerBadge, { justifyContent: 'flex-start' }]}>
-					{
-						prospecto.data &&
-						<View style={stylesProspecto.badge}>
-							<Text style={stylesProspecto.textBadge}>
-								{prospecto.data} - {prospecto.hora} {prospecto.local && `-`} {prospecto.local}
-							</Text>
-						</View>
-					}
-				</View>
 
 				{
 					prospecto.dataParaFinalizarAAcao &&
@@ -80,73 +75,128 @@ class Prospecto extends React.Component {
 						prospecto.situacao_id === SITUACAO_CADASTRO ||
 						prospecto.situacao_id === SITUACAO_MENSAGEM ||
 						prospecto.situacao_id === SITUACAO_LIGAR) &&
-					<View>
-						<Text style={{ color: '#FFFFFF' }}>
+					<View style={{
+						backgroundColor: black, borderTopStartRadius: 6, borderTopEndRadius: 6,
+						flexDirection: 'row', justifyContent: 'center', alignItems: 'center',
+						paddingVertical: 6,
+					}}>
+						<Text style={{ textAlign: 'center', color: '#FFFFFF' }}>
 							{
 								(prospecto.situacao_id === SITUACAO_IMPORTAR ||
 									prospecto.situacao_id === SITUACAO_CADASTRO) &&
-								'Voce precisa enviar uma mensagem ate '
+								'Mensagem até '
 							}
 							{
 								prospecto.situacao_id === SITUACAO_MENSAGEM &&
-								'Voce precisa ligar ate '
+								'Ligar até '
 							}
 							{
 								prospecto.situacao_id === SITUACAO_LIGAR &&
-								'Voce tem que visitar ate '
+								'Visitar até '
 							}
 							{prospecto.dataParaFinalizarAAcao}
 						</Text>
+						<View style={{ flexDirection: 'row' }}>
+							{/* <View style={{ borderRightWidth: 1, borderRightColor: gray, paddingHorizontal: 6 }}>
+								<TouchableOpacity
+									onPress={() => { this.chamarOTelefoneDoCelular() }}
+									hitSlop={{ top: 15, right: 0, bottom: 15, left: 15 }}
+								>
+									<Icon name="phone" size={20} color={white} />
+								</TouchableOpacity>
+							</View> */}
+							{/* <View style={{ paddingHorizontal: 6 }}>
+								<TouchableOpacity
+									onPress={() => { this.whatsapp() }}
+									hitSlop={{ top: 15, right: 5, bottom: 15, left: 0 }}
+								>
+									<Icon name="whatsapp" size={20} color={white} type='font-awesome' />
+								</TouchableOpacity>
+							</View> */}
+						</View>
 					</View>
 				}
 
-				<View style={stylesProspecto.containerProspecto}>
-					<View style={stylesProspecto.containerName}>
-						<View style={{ backgroundColor: lightdark, padding: 5, marginRight: 5 }}>
-							<Icon name="user" type="font-awesome" color={white} />
-						</View>
-						<Text style={[stylesProspecto.text]}>{prospecto.nome}</Text>
-					</View>
-					<View style={stylesProspecto.containerActions}>
-						<TouchableOpacity
-							style={{ padding: 5 }} onPress={() => { this.chamarOTelefoneDoCelular() }}
-							hitSlop={{ top: 15, right: 0, bottom: 15, left: 15 }}
-						>
-							<Icon name="phone" size={20} color={white} />
-						</TouchableOpacity>
-						<TouchableOpacity
-							style={{ padding: 5 }} onPress={() => { this.whatsapp() }}
-							hitSlop={{ top: 15, right: 5, bottom: 15, left: 0 }}
-						>
-							<Icon name="whatsapp" size={20} color={white} type='font-awesome' />
-						</TouchableOpacity>
-						<TouchableOpacity
-							style={{ padding: 5 }} onPress={() => funcaoOnPressDoIconeList()}
-							hitSlop={{ top: 15, right: 15, bottom: 15, left: 0 }}
-						>
-							<Icon name='list' type='font-awesome' color={white} type='font-awesome' />
-						</TouchableOpacity>
-					</View>
-				</View>
+				{/* <View style={stylesProspecto.containerProspecto}> */}
+				<>
+					<TouchableOpacity
+						style={stylesProspecto.containerProspecto}
+						onPress={() => { this.setState({ mostrarOpcoes: !mostrarOpcoes }) }}
+					>
 
-
-				<View style={[stylesProspecto.containerBadge, { borderTopWidth: 1, borderColor: gray }]}>
-					{
-						listaDeMedalhas.map(medalha =>
-							<View
-								key={prospecto._id + medalha.icone}
-								style={[stylesProspecto.containerBadgeIcons]}
-							>
-								<Icon
-									name={medalha.icone}
-									type='font-awesome'
-									color={medalha.cor}
-									size={medalha.icone === 'envelope' || medalha.icone === 'calendar' ? 26 : 28}
-								/>
+						<View style={stylesProspecto.containerName}>
+							<View style={{ flexDirection: 'row', alignItems: 'center' }}>
+								<Text numberOfLines={1} style={[stylesProspecto.text]}>{prospecto.nome}</Text>
 							</View>
-						)
-					}
-				</View>
+							{
+								prospecto.data &&
+								<View style={[stylesProspecto.containerBadge, { justifyContent: 'flex-start', paddingLeft: 23 }]}>
+									<View style={stylesProspecto.badge}>
+										<Text style={stylesProspecto.textBadge}>
+											{prospecto.data} - {prospecto.hora} {prospecto.local && `-`} {prospecto.local}
+										</Text>
+									</View>
+								</View>
+							}
+						</View>
+
+						<View style={stylesProspecto.containerActions}>
+							<TouchableOpacity
+								style={{
+									height: 40,
+									width: 40,
+									borderRadius: 40 / 2
+								}}
+								onPress={() => { this.setState({ mostrarOpcoes: !mostrarOpcoes }) }}
+								hitSlop={{ top: 15, right: 15, bottom: 15, left: 0 }}
+							>
+								<Icon name={!mostrarOpcoes ? 'angle-down' : 'angle-up'} type='font-awesome' color={white} type='font-awesome' size={26}
+									containerStyle={{ justifyContent: 'center', alignItems: 'center', flex: 1 }}
+								/>
+							</TouchableOpacity>
+						</View>
+					</TouchableOpacity>
+					{/* </View> */}
+				</>
+
+
+				{
+					mostrarOpcoes &&
+					<View style={[stylesProspecto.containerBadge, { borderTopWidth: 1, borderColor: gray, padding: 10 }]}>
+						<View style={{ paddingHorizontal: 6, backgroundColor: lightdark }}>
+							<TouchableOpacity
+								onPress={() => { }}
+								hitSlop={{ top: 15, right: 5, bottom: 15, left: 0 }}
+							>
+								<Icon name="trash" size={24} color={red} type='font-awesome' />
+							</TouchableOpacity>
+						</View>
+						<View style={{ paddingHorizontal: 6, backgroundColor: lightdark }}>
+							<TouchableOpacity
+								onPress={() => { this.chamarOTelefoneDoCelular() }}
+								hitSlop={{ top: 15, right: 0, bottom: 15, left: 15 }}
+							>
+								<Icon name="phone" size={24} color={white} />
+							</TouchableOpacity>
+						</View>
+						<View style={{ paddingHorizontal: 6, backgroundColor: lightdark }}>
+							<TouchableOpacity
+								onPress={() => { this.whatsapp() }}
+								hitSlop={{ top: 15, right: 5, bottom: 15, left: 0 }}
+							>
+								<Icon name="whatsapp" size={24} color={white} type='font-awesome' />
+							</TouchableOpacity>
+						</View>
+						<View style={{ paddingHorizontal: 6, backgroundColor: lightdark }}>
+							<TouchableOpacity
+								onPress={() => funcaoOnPressDoIconeList()}
+								hitSlop={{ top: 15, right: 5, bottom: 15, left: 0 }}
+							>
+								<Icon name="list" size={24} color={white} type='font-awesome' />
+							</TouchableOpacity>
+						</View>
+					</View>
+				}
 
 			</Card>
 		)
