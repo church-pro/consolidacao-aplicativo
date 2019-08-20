630399c58e9747bbd13c00f6fd9226abfcd6dc9a
Leonardo Pereira Magalhaes <falecomleonardopereira@gmail.com>
2019-08-20 18:43:58 -0300

    versao 70 banco

--- helpers/api.js
+++ helpers/api.js
@@ -1,6 +1,6 @@
 import { AsyncStorage } from 'react-native'
 
-const versaoBanco = '0063'
+const versaoBanco = '0070'
 const CHAVE_ADMINISTRACAO = 'churchProConsolidacao:administracao' + versaoBanco
 const CHAVE_PROSPECTOS = 'churchProConsolidacao:prospectos' + versaoBanco
 const CHAVE_USUARIO = 'churchProConsolidacao:usuario' + versaoBanco
