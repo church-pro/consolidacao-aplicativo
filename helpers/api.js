f0c57e0e865004a75b40834fa7971e126a326a67
Isaias Melo <isaiassdm@hotmail.com>
2019-08-23 15:03:53 -0300

    verificacao de clubes para exclusao

--- helpers/api.js
+++ helpers/api.js
@@ -1,6 +1,6 @@
 import { AsyncStorage } from 'react-native'
 
-const versaoBanco = '0071'
+const versaoBanco = '0074'
 // banco de teste na versao 70
 const CHAVE_ADMINISTRACAO = 'churchProConsolidacao:administracao' + versaoBanco
 const CHAVE_PROSPECTOS = 'churchProConsolidacao:prospectos' + versaoBanco
