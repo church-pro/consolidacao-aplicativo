48878e4662b6ec9e5f5586c0801b26c27235719e
Leonardo Pereira Magalhaes <falecomleonardopereira@gmail.com>
2019-10-29 18:23:17 -0300

    botao do anuncio com3 segundo

--- screens/AnuncioScreen.js
+++ screens/AnuncioScreen.js
@@ -17,52 +17,72 @@ class AnuncioScreen extends React.Component {
 		header: null,
 	}
 
+	state = {
+		mostrarBotoes: false,
+	}
+
 	bannerError = (e) => {
 		console.log('bannerError: ', e)
 	}
 
+	componentDidMount(){
+		setTimeout(() => {
+			this.setState({mostrarBotoes: true})
+		}, 3000);
+	}
+
 	render() {
+		const {
+			mostrarBotoes,
+		} = this.state
 		const {
 			navigation,
 		} = this.props
-
 		const { 
 			qualAba,
 		} = navigation.state.params
-
 		return (
 			<View style={{ flex: 1, backgroundColor: lightdark, padding: 20 }}>
 				<Text style={{ color: white, fontWeight: 'bold', textAlign: 'center', fontSize: 22, marginVertical: 16 }}>
 					Este anuncio ajuda a manter o projeto grátis
 				</Text>
-				<TouchableOpacity 
-					onPress={() => navigation.navigate('Prospectos', {qualAba})}>
-					<Text style={{ fontSize: 16, color: gray }}>
-						X
-					</Text>
-				</TouchableOpacity>
-				<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+				{
+					mostrarBotoes &&
+						<TouchableOpacity 
+							onPress={() => navigation.navigate('Prospectos', {qualAba})}>
+							<Text style={{ fontSize: 16, color: gray }}>
+								X
+							</Text>
+						</TouchableOpacity>
+				}
+				<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', padding: 10, }}>
 					<AdMobBanner
-						bannerSize="fullBanner"
+						key='banner'
+						ref={(input) => { this.banner = input; }}
+						style={{margin: 10,}}
+						bannerSize="banner"
 						adUnitID="ca-app-pub-6689947841260748/8147764780"
 						onDidFailToReceiveAdWithError={this.bannerError} />
 				</View>
-				<TouchableOpacity
-					onPress={() => console.log(1)}
-					style={{
-						width: '100%',
-						backgroundColor: primary,
-						height: 45,
-						borderRadius: 6,
-						justifyContent: 'center',
-						shadowOffset: { width: 5, height: 5, },
-						shadowColor: 'rgba(0,0,0,0.3)',
-						shadowOpacity: 1.5,
-					}} >
-					<Text style={{ textAlign: "center", fontSize: 16, color: white }}>
-						Saiba Mais
-					</Text>
-				</TouchableOpacity>
+				{
+					mostrarBotoes &&
+						<TouchableOpacity
+							onPress={() => this.banner.onPress()}
+							style={{
+								width: '100%',
+								backgroundColor: primary,
+								height: 45,
+								borderRadius: 6,
+								justifyContent: 'center',
+								shadowOffset: { width: 5, height: 5, },
+								shadowColor: 'rgba(0,0,0,0.3)',
+								shadowOpacity: 1.5,
+							}} >
+							<Text style={{ textAlign: "center", fontSize: 16, color: white }}>
+								Saiba Mais
+							</Text>
+						</TouchableOpacity>
+				}
 			</View>
 		)
 	}
